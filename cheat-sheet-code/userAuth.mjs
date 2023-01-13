8d5ba66c0c12f4e401d76b0da7d4e63d669d77aa
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-01-13 16:37:12 +0100

    Update userAuth.mjs

--- cheat-sheet-code/userAuth.mjs
+++ cheat-sheet-code/userAuth.mjs
@@ -2,18 +2,18 @@
 import express from "express";
 import dbConnect from "../Models"
 //Assigning db.users to User variable
- const User = dbConnect.users;
+const user = dbConnect.users;
 
 //Function to check if username or email already exist in the database
 //this is to avoid having two users with the same username and email
- const saveUser = async (req, res, next) => {
- //search the database to see if user exist
- try {
-   const username = await User.findOne({
-     where: {
-       userName: req.body.userName,
-     },
-   });
+const saveUser = async (req, res, next) => {
+//search the database to see if user exist
+try {
+  const username = await User.findOne({
+    where: {
+      userName: req.body.userName,
+    },
+  });
    //if username exist in the database respond with a status of 409
    if (username) {
      return res.json(409).send("username already taken");
