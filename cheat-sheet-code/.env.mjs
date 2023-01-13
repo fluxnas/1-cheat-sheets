bb452bd26cd5428dcbdff9a3aa04392ff0b47998
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-01-13 15:14:35 +0100

    Update .env.mjs

--- cheat-sheet-code/.env.mjs
+++ cheat-sheet-code/.env.mjs
@@ -1,5 +1,5 @@
 DATABASE_NAME = "name_project_db"    
 DATABASE_USER = "name_project_admin"  
 DATABASE_PASSWORD = 'sfAC83e6VXpi68'   
-DATABASE_PORT = "5432"  
-DATABASE_HOST = "localhost"  
+PORT = "5432"  
+HOST = "localhost"  
