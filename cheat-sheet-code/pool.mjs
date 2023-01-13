6d07f9a16ac18a9997f1e5593d2e4f997f437a79
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-01-13 15:12:30 +0100

    Update pool.mjs

--- cheat-sheet-code/pool.mjs
+++ cheat-sheet-code/pool.mjs
@@ -6,6 +6,6 @@ export const pool = new pg.Pool ({
 database: process.env.DATABASE_NAME,
 user: process.env.DATABASE_USER,
 password: process.env.DATABASE_PASSWORD,
-port: process.env.DATABASE_PORT,
-host: process.env.DATABASE_HOST,
+port: process.env.PORT,
+host: process.env.HOST,
 })
