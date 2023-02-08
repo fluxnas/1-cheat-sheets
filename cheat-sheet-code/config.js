41d3f97b80ba939309429e0b0f33c76df1dad8e0
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-02-08 14:08:59 +0100

    Create config.js

---
+++ cheat-sheet-code/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.PORT,
+    dialect: postgres,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    }
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: postgres,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    }
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: postgres,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
+}
