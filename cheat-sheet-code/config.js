3480bb10d7827fdf8fcb71777ce2f7311bc5c126
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-02-09 16:25:29 +0100

    Update config.js

--- cheat-sheet-code/config.js
+++ cheat-sheet-code/config.js
@@ -1,39 +1,41 @@
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.PORT,
-    dialect: postgres,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    }
+{
+  "development": {
+    "username": "process.env.DB_USERNAME",
+    "password": "process.env.DB_PASSWORD",
+    "database": "process.env.DB_NAME",
+    "host": "process.env.PORT",
+    "dialect": "postgres", 
+    "dialectOptions": { 
+      "ssl": { 
+        "rejectUnauthorized": false, 
+      } 
+    } 
   },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: postgres,
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    }
+  "test": {
+    "username": "process.env.DB_USERNAME",
+    "password": "process.env.DB_PASSWORD",
+    "database": "process.env.DB_NAME",
+    "host": "process.env.PORT",
+    "dialect": "postgres", 
+    "dialectOptions": { 
+      "ssl": { 
+        "rejectUnauthorized": false, 
+      } 
+    } 
   },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: postgres,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      }
-    }
+  "production": {
+    "username": "process.env.DB_USERNAME",
+    "password": "process.env.DB_PASSWORD",
+    "database": "process.env.DB_NAME",
+    "host": "process.env.PORT",
+    "dialect": "postgres", 
+    "dialectOptions": { 
+      "ssl": { 
+        "require": true, 
+        "rejectUnauthorized": false
+      } 
+    } 
   }
 }
+
+
