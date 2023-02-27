9831c80debdd310c4423c3215c1ee34efcc2b970
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-02-27 14:31:36 +0100

    Update migration.js

--- cheat-sheet-code/migration.js
+++ cheat-sheet-code/migration.js
@@ -1,17 +1,18 @@
-'use strict';   
+'use strict';
 
-/** @type {import('sequelize-cli').Migration} */    
-module.exports = {    
-  async up (queryInterface, Sequelize) {    
-    return Promise.all([  
-      queryInterface.addColumn('about', {  
-        type: Sequelize.STRING,  
-        allowNull: true,  
-      }),  
-    ]);  
-  },  
-  
-  async down (queryInterface, Sequelize) {  
-    return Promise.all([queryInterface.removeColumn('about')]);  
-  }  
-};  
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn('users',  //tableName
+                               'about', { //columnName
+        type: Sequelize.STRING, // definition de la column
+        allowNull: true,
+      }),
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([queryInterface.removeColumn('users', 'about')]);
+  }
+};
